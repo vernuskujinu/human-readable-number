02d143b4ba9796b3653e80cefb8135384dfdd3eb
vernuskujinu <78141473+vernuskujinu@users.noreply.github.com>
2021-03-12 15:46:59 +0300

    edit: refactor solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,62 @@
-module.exports = function toReadable (number) {
-  
-}
+module.exports = function toReadable(number) {
+    if (!number) return "zero";
+    let arr12 = [
+        "",
+        "one",
+        "two",
+        "three",
+        "four",
+        "five",
+        "six",
+        "seven",
+        "eight",
+        "nine",
+        "ten",
+        "eleven",
+        "twelve",
+        "thirteen",
+        "fourteen",
+        "fifteen",
+        "sixteen",
+        "seventeen",
+        "eighteen",
+        "nineteen",
+    ];
+    let arr100 = [
+        "",
+        "ten",
+        "twenty",
+        "thirty",
+        "forty",
+        "fifty",
+        "sixty",
+        "seventy",
+        "eighty",
+        "ninety",
+    ];
+    if (number < 20) return arr12[number];
+    if (number > 19 && number < 100) {
+        if (number % 10 === 0) return arr100[Math.floor(number / 10)];
+        else return arr100[Math.floor(number / 10)] + " " + arr12[number % 10];
+    }
+    if (number > 99) {
+        if (number % 100 === 0)
+            return arr12[Math.floor(number / 100)] + " " + "hundred";
+        if (number % 10 === 0)
+            return (
+                arr12[Math.floor(number / 100)] +
+                " " +
+                "hundred" +
+                " " +
+                arr100[(number % 100) / 10]
+            );
+        else
+            return (
+                arr12[Math.floor(number / 100)] +
+                " " +
+                "hundred" +
+                " " +
+                toReadable(number % 100)
+            );
+    }
+};
